957c1c1e01e73eb7532a0700c1141a1c3ca1022a
Bezruchko Anastasia <anastasiabezruchko17@gmail.com>
2022-10-03 21:52:24 +0300

    fix indentation

--- test_product_page.py
+++ test_product_page.py
@@ -3,12 +3,12 @@ import time
 from pages.product_page import ProductPage
 from pages.basket_page import BasketPage
 
- def test_guest_can_add_product_to_the_basket(browser):
-     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-     page = ProductPage(browser, link)  
-     page.open()                   
-     page.add_product_to_the_basket()
-     page.solve_quiz_and_get_code()   
+def test_guest_can_add_product_to_the_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    page = ProductPage(browser, link)  
+    page.open()                   
+    page.add_product_to_the_basket()
+    page.solve_quiz_and_get_code()   
 
 def test_product_with_correct_name_added_to_the_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
@@ -38,6 +38,7 @@ def test_product_with_correct_price_added_to_the_basket(browser):
 
     basket_url = "http://selenium1py.pythonanywhere.com/ru/basket/"
     basket = BasketPage(browser, basket_url)
+
     basket.open()
     product_price_on_basket_page = basket.get_added_product_price()
 

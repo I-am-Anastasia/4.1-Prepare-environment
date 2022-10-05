cad965d2be0f949b7feea6a3dd1592d916477f16
Bezruchko Anastasia <anastasiabezruchko17@gmail.com>
2022-10-06 02:06:40 +0300

    add parametrize

--- test_product_page.py
+++ test_product_page.py
@@ -1,39 +1,52 @@
-import time
+import re
+import pytest
+
 
 from pages.product_page import ProductPage
 from pages.basket_page import BasketPage
 
-def test_guest_can_add_product_to_the_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    page = ProductPage(browser, link)  
-    page.open()                   
-    page.add_product_to_the_basket()
-    page.solve_quiz_and_get_code()   
+# def test_guest_can_add_product_to_the_basket(browser):
+#     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+#     page = ProductPage(browser, link)  
+#     page.open()                   
+#     page.add_product_to_the_basket()
+#     page.solve_quiz_and_get_code()   
 
-def test_product_with_correct_name_added_to_the_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    page = ProductPage(browser, link)  
-    page.open()                   
-    page.add_product_to_the_basket()
-    page.solve_quiz_and_get_code() 
+# def test_product_with_correct_name_added_to_the_basket(browser):
+#     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+#     page = ProductPage(browser, link)  
+#     page.open()                   
+#     page.add_product_to_the_basket()
+#     page.solve_quiz_and_get_code() 
 
-    product_name_on_product_page = page.get_product_name()
+#     product_name_on_product_page = page.get_product_name()
 
-    basket_url = "http://selenium1py.pythonanywhere.com/ru/basket/"
-    basket = BasketPage(browser, basket_url)
+#     basket_url = "http://selenium1py.pythonanywhere.com/ru/basket/"
+#     basket = BasketPage(browser, basket_url)
     
-    basket.open()
-    product_name_on_basket_page = basket.get_added_product_name()
+#     basket.open()
+#     product_name_on_basket_page = basket.get_added_product_name()
 
-    assert product_name_on_basket_page == product_name_on_product_page
+#     assert product_name_on_basket_page == product_name_on_product_page
 
-def test_product_with_correct_price_added_to_the_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+product_base_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+urls = [f"{product_base_link}/?promo=offer{no}" for no in range(10)]
+urls[7] = pytest.param(urls[7], marks=pytest.mark.xfail)
+
+#@pytest.mark.parametrize('link', urls)
+# def test_guest_can_add_product_to_basket(browser, link):   
+#     page = ProductPage(browser, link)  
+#     page.open()                  
+#     page.add_product_to_the_basket()
+#     page.solve_quiz_and_get_code()
+
+@pytest.mark.parametrize('link', urls)
+def test_product_with_correct_price_added_to_the_basket(browser, link):
     page = ProductPage(browser, link)  
     page.open()                   
     page.add_product_to_the_basket()
     page.solve_quiz_and_get_code() 
-
+    
     product_price_on_product_page = page.get_product_price()
 
     basket_url = "http://selenium1py.pythonanywhere.com/ru/basket/"
@@ -42,6 +55,11 @@ def test_product_with_correct_price_added_to_the_basket(browser):
     basket.open()
     product_price_on_basket_page = basket.get_added_product_price()
 
-    assert product_price_on_basket_page == product_price_on_product_page
-    
+    expression = '[\d\s.,]*\d'
 
+    price1 = re.search(expression, product_price_on_basket_page).group(0)
+    price1 = price1.replace(',', '.')
+    price2 = re.search(expression, product_price_on_product_page).group(0)
+   
+    assert price1 == price2
+    
